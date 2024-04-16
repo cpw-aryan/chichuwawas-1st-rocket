753b61e10136d726f8fed4181a059ba86f0d4b4e
cpw-aryan <167211005+cpw-aryan@users.noreply.github.com>
2024-04-16 19:00:40 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,61 @@
+basic.forever(function () {
+    basic.showIcon(IconNames.Heart)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.showLeds(`
+        . . # . .
+        # # # # #
+        # . # . #
+        # . # . #
+        # . # . #
+        `)
+    basic.showLeds(`
+        . . # . .
+        # # # # #
+        # . # . #
+        # . # . #
+        # . # . #
+        `)
+    basic.showLeds(`
+        # # # # #
+        # . # . #
+        # . # . #
+        # . # . #
+        . . . . .
+        `)
+    basic.showLeds(`
+        # # # # #
+        # . # . #
+        # . # . #
+        # . # . #
+        . . . . .
+        `)
+    basic.showLeds(`
+        # . # . #
+        # . # . #
+        # . # . #
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # . # . #
+        # . # . #
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # . # . #
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showString("Chichuwawa's 1st rocket")
+})
